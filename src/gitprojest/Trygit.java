d0f6a2a6b7259447a239d6d609f8d9399bb4b696
julietrajam <juliet.ramasamy@gmail.com>
2018-10-29 16:02:27 +0530

    Modified

--- src/gitprojest/Trygit.java
+++ src/gitprojest/Trygit.java
@@ -1,5 +1,17 @@
 package gitprojest;
 
 public class Trygit {
-
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+public void sum()
+{
+	System.out.println("Welcome");
+}
+	public static void main(String[] args) {
+		Trygit o1=new Trygit();
+		o1.add(10, 20);
+		o1.sum(;)
+	}
 }
